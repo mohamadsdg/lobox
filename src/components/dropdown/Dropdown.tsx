7b600b53dd0277644c5d4c03357b4e744d291cb4
mohamad_sdg <mohamad.r.sadeghi93@gmail.com>
2023-03-16 18:09:18 +0330

    style: work on label

--- src/components/dropdown/Dropdown.tsx
+++ src/components/dropdown/Dropdown.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 import { createUseStyles } from "react-jss";
 import useDropdown from "../../hooks/useDropdown";
-const useStyles = createUseStyles({});
+
+const useStyles = createUseStyles({
+  root: {},
+  label: {
+    background: "#ffffff",
+    border: "1px solid #ced4da",
+    transition: "ease 0.2s",
+    borderRadius: "6px",
+    padding: "0.75rem 0.75rem",
+    "&:focus": {
+      boxShadow: "0 0 0 0.2rem #C7D2FE",
+      borderColor: "#6366F1",
+    },
+  },
+  list: {},
+  item: {},
+});
 
 type DropdownListType = {
   list?: Array<string>;
@@ -14,6 +30,8 @@ const DropdownList: React.FC<DropdownListType> = ({
   defaultOpen,
   defaultLabel,
 }): React.ReactElement => {
+  const classes = useStyles();
+
   const { containerRef, isOpen, toggling, close } = useDropdown(defaultOpen);
   const [selectedOption, setSelectedOption] = React.useState<
     string | undefined
@@ -27,14 +45,20 @@ const DropdownList: React.FC<DropdownListType> = ({
   };
 
   return (
-    <div ref={containerRef}>
-      <button onClick={toggling}>{selectedOption}</button>
+    <div ref={containerRef} className={classes.root}>
+      <div onClick={toggling} className={classes.label}>
+        {selectedOption}
+      </div>
       {isOpen && (
-        <div>
+        <div className={classes.list}>
           <ul>
             {!options && <li>empty</li>}
             {options?.map((option) => (
-              <li onClick={onSelect(option)} key={Math.random()}>
+              <li
+                className={classes.item}
+                onClick={onSelect(option)}
+                key={Math.random()}
+              >
                 {option}
               </li>
             ))}
